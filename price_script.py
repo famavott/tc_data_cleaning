fc4f99b494a9db4f2a9c43ecc3fc34e0b1529412
Matt Favoino <mattfavoino@gmail.com>
2018-05-30 18:10:32 -0500

    Refine for further cleaning.

--- price_script.py
+++ price_script.py
@@ -16,6 +16,30 @@ def clean_and_write_prices(in_csv, out_csv):
         print('Output complete')
 
 
+# def separate_sku(in_csv, out_csv):
+#     """Separate sku from product description, then write to output csv."""
+#     with open(in_csv) as in_file, open(out_csv, 'w') as out_file:
+#         reader = csv.reader(in_file)
+#         # writer = csv.writer(out_file)
+#         # writer.writerow(['customer', 'sku', 'description', 'price'])
+
+
+def remove_dashes(in_csv, out_csv):
+    """Remove dashes for data cleaning by customer."""
+    with open(in_csv) as in_file, open(out_csv, 'w') as out_file:
+        reader = csv.reader(in_file)
+        writer = csv.writer(out_file)
+        writer.writerow(['customer_id', 'customer', 'product', 'price', 'sku'])
+
+        for row in reader:
+            new_index_0 = row[0].split('-', 1)[0]
+            new_index_1 = row[0].split('-', 1)[-1]
+            writer.writerow([new_index_0, new_index_1, row[1], row[2].strip(), row[3]])
+        print('All done')
+
+
 if __name__ == '__main__':
     # change file names when calling function below and make sure paths are correct
-    clean_and_write_prices('csv/Clean_Pricing_Tab_2.csv', 'csv/out.csv')
+    # clean_and_write_prices('csv/Clean_Pricing_Tab_2.csv', 'csv/out.csv')
+    # separate_sku('csv/Clean_Pricing_ALL.csv', 'csv/out.csv')
+    remove_dashes('csv/Pricing_by_customer.csv', 'csv/output_no_dashes.csv')
